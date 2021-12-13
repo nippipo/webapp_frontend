7954a81ee5302274cf267f45b00d7b6552edc549
nippipo <genmeozuro@gmail.com>
2021-12-13 17:18:07 +0100

    in progress

---
+++ src/views/AfterSaveEdit.vue
@@ -0,0 +1,15 @@
+<template>
+  <ul>
+      <li v-for="product in products" :key="product.productsName"></li>
+      <product :count="product.stock" :pgname="product.productsName" :pgprice="product.price" :pgimage="product.imageurl" /> 
+  </ul>
+</template>
+
+<script>
+import Product from '@/components/Product.vue' 
+import Product from '../components/Product.vue';
+export default {
+  name: "UserView",
+};
+    Product
+</script>
