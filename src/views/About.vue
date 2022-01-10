7a89903b32bc2b50ef8e2b67dde4accb212f5d9a
nippipo <genmeozuro@gmail.com>
2022-01-10 12:06:37 +0100

    for sale

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="about">
     <h1>Welcome to webapp for aliens</h1>
-    <h2>Please log in to see sample products and post your own ones for sales</h2>
+    <h2>Please log in to see sample products and post your own ones for sale</h2>
   </div>
 </template>
 
